11e423aeb4e6ceb4bf8fee33344bcc42c30d59b4
Utkarsh Roy <123242625+im-ukr@users.noreply.github.com>
2024-07-23 15:56:55 +0530

    updated

--- sql/hms.sql
+++ sql/hms.sql
@@ -1,21 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.26, for macos11.4 (arm64)
---
--- Host: mohityadav.codes    Database: hms
--- ------------------------------------------------------
--- Server version	5.5.5-10.5.12-MariaDB-0ubuntu0.21.04.1
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
 -- Table structure for table `guests`
 --
 
