6dcad11e5d3ff759cb7ce27e8469de512b89ebd7
Utkarsh Roy <123242625+im-ukr@users.noreply.github.com>
2024-08-30 16:10:48 +0530

    Create ex_db.config.py

---
+++ Model/ex_db.config.py
@@ -0,0 +1,12 @@
+# Example content of db.config.py:
+
+# db_config.py
+username = 'root'
+password = 'pass'
+host = 'localhost'
+port = '3306'
+database = 'smartstay'
+
+# mail credentials
+email="email-id"
+passw="password"
