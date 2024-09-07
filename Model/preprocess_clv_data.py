47dab0ff95280b13dd5b961c7db5f043c604e062
Utkarsh Roy <123242625+im-ukr@users.noreply.github.com>
2024-09-07 17:03:09 +0530

    updated with avg stay duration + meal charges

--- Model/preprocess_clv_data.py
+++ Model/preprocess_clv_data.py
@@ -4,7 +4,6 @@ import pandas as pd
 from datetime import datetime
 
 def process_clv_data():
-
     # Directory for saving CSV files
     csv_folder = 'csv_files'
 
@@ -25,16 +24,24 @@ def process_clv_data():
     # Step 1: Compute frequency of bookings for each guest
     df1['frequency_of_bookings'] = df1.groupby('guest_id')['guest_id'].transform('count')
 
-    # Calculate time since last booking based on the latest check_out_date in the dataset
+    # Step 2: Calculate time since last booking based on the latest check_out_date in the dataset
     df1['days_since_last_booking'] = df1.groupby('guest_id')['check_out_date'].transform(
-    lambda x: (pd.to_datetime(current_time) - x.iloc[-1]).days if pd.notna(x.iloc[-1]) else None
+        lambda x: (pd.to_datetime(current_time) - x.iloc[-1]).days if pd.notna(x.iloc[-1]) else None
     )
 
     # Step 3: Calculate total revenue generated for each guest
     df1['total_revenue_generated'] = df1.groupby('guest_id')['grand_total_amount'].transform('sum')
 
+    # Step 4: Calculate average stay duration for each guest
+    df1['average_stay_duration'] = df1.groupby('guest_id')['duration_of_stay'].transform('mean')
+
+    # Step 5: Calculate total amount spent on meals by each guest
+    df1['total_meal_charges'] = df1.groupby('guest_id')['meal_charges'].transform('sum')
+
     # Updated dataframe
-    result_df = df1[['guest_id', 'guest_name', 'email_id','frequency_of_bookings', 'days_since_last_booking', 'total_revenue_generated']].drop_duplicates().reset_index(drop=True)
+    result_df = df1[['guest_id', 'guest_name', 'email_id', 'frequency_of_bookings', 
+                     'days_since_last_booking', 'total_revenue_generated', 
+                     'average_stay_duration', 'total_meal_charges']].drop_duplicates().reset_index(drop=True)
 
     # Save the modified dataframe to csv_files directory
     result_df.to_csv(os.path.join(csv_folder, 'condensed-clv-data.csv'), index=False)
